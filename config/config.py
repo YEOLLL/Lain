1491c9091c819a2498ad23b79ee53aa16de036df
YEOLLL <28512134+YEOLLL@users.noreply.github.com>
2021-09-11 16:14:41 +0800

    配置文件

---
+++ config/config.py
@@ -0,0 +1,24 @@
+config = {
+    # 代理
+    "proxies": {
+        "https": ""
+    },
+
+    # 推特
+    "Twitter": {
+        "account": {
+            "username": "",
+            "password": ""
+        },
+        "user_list": [""],
+        "have_to_login": True,
+        "use_proxies": True
+    },
+
+    # 网易云
+    "NetEaseMusic": {
+        "user_list": [""],
+        "have_to_login": False,
+        "use_proxies": False
+    }
+}
