1f495053ddb8ff58e9b89e27604b98ddf01ccfc0
YEOLLL <28512134+YEOLLL@users.noreply.github.com>
2021-09-16 14:21:18 +0800

    配置文件

--- config/config.py
+++ config/config.py
@@ -1,8 +1,11 @@
 config = {
     # 代理
     "proxies": {
-        "https": ""
+        "https": "http://127.0.0.1:7890"
     },
+
+    # 启用的模块
+    "modules_enabled": ["Twitter", "NetEaseMusic", "Github", "DoubanGroup"],
 
     # 推特
     "Twitter": {
@@ -10,13 +13,27 @@ config = {
             "username": "",
             "password": ""
         },
-        "user_list": [""],
+        "user_list": ["CharXD"],
         "have_to_login": True,
         "use_proxies": True
     },
 
     # 网易云
     "NetEaseMusic": {
+        "user_list": [""],
+        "have_to_login": False,
+        "use_proxies": False
+    },
+
+    # Github
+    "Github": {
+        "user_list": [""],
+        "have_to_login": False,
+        "use_proxies": True
+    },
+
+    # 豆瓣小组
+    "DoubanGroup": {
         "user_list": [""],
         "have_to_login": False,
         "use_proxies": False
