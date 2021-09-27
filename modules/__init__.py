9edec4c13180fceddaff34b2ecb1d9f787e20848
YEOLLL <28512134+YEOLLL@users.noreply.github.com>
2021-09-27 19:59:30 +0800

    添加酷安模块

--- modules/__init__.py
+++ modules/__init__.py
@@ -3,6 +3,7 @@ from .twitter import Twitter
 from .netease_music import NetEaseMusic
 from .github import Github
 from .douban_group import DoubanGroup
+from .coolapk import Coolapk
 
 # 日志
 from .log import logger
