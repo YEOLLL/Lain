3e376290c56dbd347a788b51c416a757ee80786c
YEOLLL <28512134+YEOLLL@users.noreply.github.com>
2021-09-27 19:57:25 +0800

    添加模块，优化输出样式

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from modules import *
 from config import *
-
 from os import mkdir, path
 
 
@@ -8,7 +7,10 @@ class MainApp:
     def __init__(self, output_path="./output"):
         self.modules = {
             "Twitter": Twitter,
-            "NetEaseMusic": NetEaseMusic
+            "NetEaseMusic": NetEaseMusic,
+            "Github": Github,
+            "DoubanGroup": DoubanGroup,
+            "Coolapk": Coolapk
         }
         for k, v in self.modules.items():
             setattr(self, k, v)
@@ -26,8 +28,12 @@ if __name__ == "__main__":
     # And you don't even feel a thing
 
     main_app = MainApp()
-    for module_name in main_app.modules.keys():
+    modules_enabled = config["modules_enabled"]
+    if modules_enabled == "All":
+        modules_enabled = main_app.modules.keys()
 
+    # for module_name in main_app.modules.keys():
+    for module_name in modules_enabled:
         # 每个模块的结果存储到各自的文件夹
         module_output_path = path.join(main_app.output_path, module_name)
         if not path.exists(module_output_path):
@@ -35,15 +41,12 @@ if __name__ == "__main__":
 
         # 获取模块
         attr = getattr(main_app, module_name)
+        module = attr()
 
         # 需要登录则传入配置中的账户
         if config[module_name]["have_to_login"]:
-            module = attr(
-                config[module_name]["account"]["username"],
-                config[module_name]["account"]["password"],
-            )
-        else:
-            module = attr()
+            module.set_login_username(config[module_name]["account"]["username"])
+            module.set_login_password(config[module_name]["account"]["password"])
 
         # 设置代理，如果配置了的话
         if config[module_name]["use_proxies"]:
@@ -53,9 +56,25 @@ if __name__ == "__main__":
         module.set_user_list(config[module_name]["user_list"])
 
         # 获取全部粉丝
-        followers_dict = module.get_all_followers()
-        for user, followers in followers_dict.items():
-            # 为每个用户创建单独的文件
-            user_path = path.join(module_output_path, f"{user}.txt")
-            with open(user_path, 'w') as f:
-                f.write(str(followers))
+        logger.info("正在启用模块：{}", module_name)
+
+        if not (followers_dict := module.get_all_followers()):
+            logger.warning("模块运行出错，跳过并开始运行下一模块")
+
+        else:
+            for user, followers in followers_dict.items():
+                # 为每个用户创建单独的文件
+                user_path = path.join(module_output_path, f"{user}.txt")
+                with open(user_path, 'w') as f:
+                    f.write(str(followers))
+            logger.info("模块运行完毕：{}", module_name)
+
+        # 分割线
+        logger.opt(
+            raw=True,
+            colors=True,
+            record=True
+        ).info(
+            "<level>[{record[level]}]</level> ----------------------------------------------------------------\n"
+        )
+
