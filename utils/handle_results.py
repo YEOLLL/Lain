8a52bf9eb12c04b1ce460a4408eec15398598ea2
YEOLLL <28512134+YEOLLL@users.noreply.github.com>
2021-10-16 22:11:06 +0800

    用来捕获结果中异常的模块

---
+++ utils/handle_results.py
@@ -0,0 +1,44 @@
+from utils.log import logger
+from utils.exception import *
+
+
+def handle_results(results, account, msg):
+    has_error = False
+    error_account = []
+    return_results = []
+    for index, value in enumerate(results):
+        if isinstance(value, KeyError):
+            logger.error('{}错误，KeyError：{}，Account：{}', msg, value, account[index])
+            has_error = True
+            error_account.append(account[index])
+
+        elif isinstance(value, HTTPError):
+            logger.error('{}错误，请求失败：{}，Account：{}', msg, value, account[index])
+            has_error = True
+            error_account.append(account[index])
+
+        elif isinstance(value, JSONDecodeError):
+            logger.error('{}错误，Json解析失败：{}，Account：{}', msg, value, account[index])
+            has_error = True
+            error_account.append(account[index])
+
+        elif isinstance(value, NotAllowToGet):
+            logger.error('{}错误，由于该用户隐私设置，粉丝列表不可见，Account：{}', msg, account[index])
+            has_error = True
+            error_account.append(account[index])
+
+        else:
+            return_results.append(value)
+
+    if has_error:
+        logger.warning(
+            '{}完毕，共 {} 个账户，其中 {} 个账户发生了错误，ErrorAccount：{}',
+            msg, len(account), len(error_account), error_account
+        )
+    else:
+        logger.success(
+            '{}完毕，共 {} 个账户',
+            msg, len(account)
+        )
+
+    return return_results
