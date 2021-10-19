21234741d2dddffea6f24c0f0b712770847214d3
YEOLLL <28512134+YEOLLL@users.noreply.github.com>
2021-10-19 15:54:51 +0800

    对配置文件进行解释

--- config/config.py
+++ config/config.py
@@ -1,59 +1,59 @@
 config = {
     # 代理
-    "proxies": {
-        "https": "http://127.0.0.1:7890"
+    'proxies': {
+        'https://': 'http://127.0.0.1:7890'
     },
 
     # 启用的模块
-    "modules_enabled": [],
+    'modules_enabled': ['CoolAPK'],
 
     # 推特
-    "Twitter": {
-        "account": {
-            "username": "",
-            "password": ""
+    'Twitter': {
+        'account': {
+            'username': '',
+            'password': ''
         },
-        "user_list": [""],
-        "have_to_login": True,
-        "use_proxies": True
+        'user_list': [''],
+        'have_to_login': True,  # 必选
+        'use_proxies': True
     },
 
     # 网易云
-    "NetEaseMusic": {
-        "user_list": [""],
-        "have_to_login": False,
-        "use_proxies": False
+    'NetEaseMusic': {
+        'user_list': [''],
+        'have_to_login': False,  # 不可选
+        'use_proxies': False
     },
 
     # Github
-    "Github": {
-        "user_list": [""],
-        "have_to_login": False,
-        "use_proxies": True
+    'Github': {
+        'user_list': [''],
+        'have_to_login': False,  # 不可选
+        'use_proxies': True
     },
 
     # 豆瓣小组
-    "DoubanGroup": {
-        "user_list": [""],
-        "account": {
-            "username": "",
-            "password": ""
+    'DoubanGroup': {
+        'user_list': [''],
+        'account': {
+            'username': '',
+            'password': ''
         },
-        "have_to_login": False,  # 可选
-        "use_proxies": False
+        'have_to_login': False,  # 可选
+        'use_proxies': False
     },
 
     # 酷安
-    "CoolAPK": {
-        "user_list": [""],
-        "have_to_login": False,
-        "use_proxies": False
+    'CoolAPK': {
+        'user_list': [''],
+        'have_to_login': False,  # 不可选
+        'use_proxies': False
     },
 
     # 抖音
-    "Douyin": {
-        "user_list": [''],
-        "have_to_login": False,
-        "use_proxies": False
+    'Douyin': {
+        'user_list': [''],
+        'have_to_login': False,  # 不可选
+        'use_proxies': False
     }
 }
